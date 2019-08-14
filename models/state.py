d5e0d9ac875a32d382cdd1f41ee7353cb04620d2
Nate Petersen <nate.petersen@me.com>
2019-08-14 00:41:25 +0000

    First draft of states converting to sqlalchemy

--- models/state.py
+++ models/state.py
@@ -1,11 +1,21 @@
 #!/usr/bin/python3
 """This is the state class"""
 from models.base_model import BaseModel
+from sqlalchemy.orm import relationship
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """This is the class for State
     Attributes:
         name: input name
     """
-    name = ""
+
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    cities = relationship(
+        'City', backref='state', cascade='all, delete, delete-orphan')
+
+    @property
+    def cities(self):
+        """returns list of Cities and some relationships"""
+        return self.cities
