a35fd7ecf8dc95e6c22ec2347db05e6f9287be99
klitscher <672@holbertonschool.com>
2019-08-14 22:01:52 +0000

    Adding if

--- models/review.py
+++ models/review.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """This is the review class"""
+import os
 from models.base_model import BaseModel, Base, Column, String
 from sqlalchemy.orm import relationship
+from sqlalchemy import ForeignKey
 
 
 class Review(BaseModel):
@@ -12,8 +14,15 @@ class Review(BaseModel):
         text: review description
     """
     __tablename__ = 'reviews'
-    text = Column(String(1024), nullable=False)
-    place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
-    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-    user = relationship(
-        'User', back_populates='reviews') # cascade?
+
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        text = Column(String(1024), nullable=False)
+        place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+        user = relationship(
+            'User', back_populates='reviews') # cascade?
+
+    else:
+        text = ""
+        place_id = ""
+        user_id = ""
