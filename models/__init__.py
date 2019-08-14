1013eb3e89193318be983f46c70258f41d6e1085
klitscher <672@holbertonschool.com>
2019-08-14 19:39:59 +0000

    Updating for db storage and to work again

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """create a unique FileStorage instance for your application"""
-from models.engine.file_storage import FileStorage
+import os
 from models.base_model import BaseModel
 from models.user import User
 from models.state import State
@@ -10,5 +10,11 @@ from models.place import Place
 from models.review import Review
 
 
-storage = FileStorage()
-storage.reload()
+if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+    storage.reload()
+else:
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
+    storage.reload()
--- models/city.py
+++ models/city.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """This is the city class"""
 
-
+import os
 from models.base_model import BaseModel, Base, Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
@@ -13,6 +14,12 @@ class City(BaseModel, Base):
     """
 
     __tablename__ = 'cities'
-    state_id = ""
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        state = relationship(
+            'State', back_populates='cities')
+    else:
+        name = ""
+        state_id = ""
