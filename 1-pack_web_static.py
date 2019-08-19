b8c175e70d64a6d53a360fe17f41ac5d9a562582
klitscher <672@holbertonschool.com>
2019-08-19 22:27:13 +0000

    Add file for fabric practice

---
+++ 1-pack_web_static.py
@@ -0,0 +1,13 @@
+#!usr/bin/python3
+"""Module to create a tar with Fabric"""
+
+import fabric
+
+
+def do_pack():
+    """Generates the tar file"""
+    date = fabric.api.local('echo $(date +%Y%m%d%H%M%S)', capture=True)
+    fabric.api.local('mkdir -p versions')
+    fabric.api.local(
+        'tar -cvzf versions/web_static_{}.tgz web_static'.format(date))
+    return('versions/web_static_{}.tgz'.format(date))
