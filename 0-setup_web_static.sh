ef7e4f9f1c0dd7986f586755e7bc08f4ba32e7a3
klitscher <672@holbertonschool.com>
2019-08-19 19:28:18 +0000

    Udpating to point to correct directory

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -11,5 +11,5 @@ mkdir -p /data/web_static/releases/test/
 echo "This is a test" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
-sed -i 's|server_name _;|server_name _;\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/hbnb_static;\n\t}\n|' /etc/nginx/sites-available/default
+sed -i 's|server_name _;|server_name _;\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n|' /etc/nginx/sites-available/default
 service nginx restart
