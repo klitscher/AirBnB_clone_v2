b57201e20e17bbf10372802bb95e45d1a940628d
klitscher <672@holbertonschool.com>
2019-08-20 01:51:54 +0000

    Readding the user and key

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,8 +5,8 @@ import fabric
 import os
 
 fabric.api.env.hosts = ['35.237.201.94', '35.231.178.196']
-# fabric.api.env.user = 'ubuntu'
-# fabric.api.env.key_file = '~/.ssh/holberton'
+fabric.api.env.user = 'ubuntu'
+fabric.api.env.key_file = '~/.ssh/holberton'
 # fabric.api.env.hosts = ['172.31.54.208:45962']
 # fabric.api.env.user = 'root'
 # fabric.api.env.password = '638f151ec8fd4a571edf'
