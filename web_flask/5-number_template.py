a1a65a93ebb0b15bb3884cd98d9d07052e06b2e4
klitscher <672@holbertonschool.com>
2019-08-29 22:30:07 +0000

    Updating all files to be executable, adding files and directory for exercise 5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -2,7 +2,7 @@
 """Module to start a Flask web application"""
 
 from flask import Flask
-
+from flask import render_template
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -26,6 +26,25 @@ def variables(text):
     text = text.replace('_', ' ')
     return "C {}".format(text)
 
+
+@app.route('/python/')
+@app.route('/python/<text>')
+def python_variable(text='is cool'):
+    """Function to display more variable text"""
+    text = text.replace('_', ' ')
+    return "Python {}".format(text)
+
+
+@app.route('/number/<int:n>')
+def number_variable(n):
+    """Returns number if it is an int"""
+    return "{} is a number".format(n)
+
+
+@app.route('/number_template/<int:n>')
+def html_variable(n):
+    """Injects a variable into html page"""
+    return render_template('5-number.html', n=n)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
