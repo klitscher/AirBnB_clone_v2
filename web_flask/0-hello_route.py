c162e4f38d4056ab3e6fc69563b4fe9550924c4e
klitscher <672@holbertonschool.com>
2019-08-29 20:20:23 +0000

    Update to move run into correct position

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,10 +6,12 @@ from flask import Flask
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
-app.run(host='0.0.0.0')
 
 
 @app.route('/')
 def hello_world():
     """Function to print something"""
     return "Hello HBNB!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
