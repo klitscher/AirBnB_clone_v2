0c4e72e5b599fba98787ed5ba7b293188fd91f12
klitscher <672@holbertonschool.com>
2019-09-02 14:15:19 -0400

    Updating to remove spaces

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -7,7 +7,7 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes = False)
+@app.route('/', strict_slashes=False)
 def hello_world():
     """Function to print something"""
     return "Hello HBNB!"
